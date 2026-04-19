83baf14dfd16966d0e227a656d4dc9780d7bea2a
Khanh23-code <nguyendangkhanh762006@gmail.com>
2026-04-19 14:35:05 +0700

    feat(compose): create ui for compose screen

--- app/src/main/java/com/soulmate/app/ui/journal/editor/MultimediaEditor.kt
+++ app/src/main/java/com/soulmate/app/ui/journal/editor/MultimediaEditor.kt
@@ -1,7 +1,50 @@
 package com.soulmate.app.ui.journal.editor
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.soulmate.app.ui.theme.BackgroundMain
 
 @Composable
 fun MultimediaEditor() {
+    var title by remember { mutableStateOf("") }
+    var content by remember { mutableStateOf("") }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = BackgroundMain
+    ) { paddingValues ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            DiaryTitleField(
+                title = title,
+                onTitleChange = { newTitle -> title = newTitle },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            DiaryContentField(
+                content = content,
+                onContentChange = { newContent -> content = newContent },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            )
+
+            // more feat: album -> insert images, voice, save button, bottom navigator
+        }
+    }
 }
