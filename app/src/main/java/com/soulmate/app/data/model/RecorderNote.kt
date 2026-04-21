6a5cab6017e94e0855a4851b014cd241593628b9
Dmanhz <duymanhbui305@gmail.com>
2026-04-21 13:18:40 +0700

    mic_ux

--- app/src/main/java/com/soulmate/app/data/model/RecorderNote.kt
+++ app/src/main/java/com/soulmate/app/data/model/RecorderNote.kt
@@ -1,7 +1,11 @@
 package com.soulmate.app.data.model
 
+import com.soulmate.app.R
+
 data class RecordedNote(
     val id: Long = System.currentTimeMillis(),
+    val dateTime: String,
     val text: String,
-    val dateTime: String
+    val userName: String = "Dmanhz",
+    val avatarRes: Int = R.drawable.ava1 // Đảm bảo có ảnh này trong res/drawable
 )
